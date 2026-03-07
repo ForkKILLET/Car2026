f3da44f24e65fc16c26da5e396b83710ca22dd36
ForkΨKILLET <fork_killet@outlook.com>
2026-03-07 13:24:42 +0800

    build(scripts/upload): upload with optional executable name

--- project/scripts/upload.sh
+++ project/scripts/upload.sh
@@ -6,6 +6,10 @@ source $(dirname $0)/config.sh
 
 # entry
 
-task:start Uploading executable
-scp ${BUILD_DIR}/final-phase ${SSH_HOST}:${SSH_PATH}/ || task:fail
+MAIN_EXECUTABLE=final-phase
+EXECUTABLE=${1:-${MAIN_EXECUTABLE}}
+UPLOAD_URL=${SSH_HOST}:${SSH_PATH}/${EXECUTABLE}
+
+task:start Uploading executable to ${UPLOAD_URL}
+scp ${BUILD_DIR}/${MAIN_EXECUTABLE} ${UPLOAD_URL} || task:fail
 task:end
